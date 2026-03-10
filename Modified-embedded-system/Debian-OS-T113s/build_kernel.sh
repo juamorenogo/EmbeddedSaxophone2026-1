bfc7fa6d0ba27456ba1f6a9ef5ab6e02e2b067a2
SebastianArch <juamorenogo@unal.edu.co>
2026-03-10 03:05:53 -0500

    Kernel-built-1

--- Modified-embedded-system/Debian-OS-T113s/build_kernel.sh
+++ Modified-embedded-system/Debian-OS-T113s/build_kernel.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+set -e # Se detendra si un comando no sirve
+
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+cd linux
+git reset --hard
+git clean -fd
 
 cd $SCRIPT_DIR
 
 cp linux-patch-6.16.9/sun8i-t113s-saxo-gateway.dts linux/arch/arm/boot/dts/allwinner
 cp linux-patch-6.16.9/sunxi-d1s-t113s-saxo.dtsi     linux/arch/arm/boot/dts/allwinner
+cp linux-patch-6.16.9/sunxi-d1s-t113.dtsi     linux/arch/riscv/boot/dts/allwinner # Linea nueva añadida para los pines
 cp linux-patch-6.16.9/config  linux/.config
 
 cd linux
 
-git checkout -f
+# N ignore los parches ya aplicados
+# patch -N -d . -p1 < ../linux-patch-6.16.9/0001-saxo-dtb-reference.patch
 
-patch -d . -p1 < ../linux-patch-6.16.9/0001-saxo-dtb-reference.patch
-
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- menuconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- olddefconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- zImage dtbs modules -j4
 
 cd $SCRIPT_DIR
